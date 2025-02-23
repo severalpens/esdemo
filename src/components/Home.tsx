d83550ca517b4a10cdfc64f57951e385d1bfbbcb
Paul Collins <paul.collins@severalpens.com>
2025-02-23 11:26:28 +1100

    chore: simplify layout structure in Home component

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -3,7 +3,7 @@ import { NavLink } from 'react-router-dom';
 // App.js
 function Home() {
     return (
-      <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <div className=" pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
         <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
           <div id="h1-title" className="flex flex-col items-center sm:items-start gap-4">
             <h1 className="text-4xl font-bold text-center sm:text-left">Elasticsearch Research</h1>
